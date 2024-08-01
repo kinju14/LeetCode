29c17951dc8248884036122f37ae946d43e299d3
Kinjal Patel <kpatel.riki@gmail.com>
2024-08-01 17:17:50 +0000

    Sync LeetCode submission Runtime - 135 ms (18.63%), Memory - 17 MB (100.00%)

---
+++ 1392-find-the-difference-of-two-arrays/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        l1 = set(nums1)
+        l2 = set(nums2)
+        return [list(l1.difference(l2)), list(l2.difference(l1))]
