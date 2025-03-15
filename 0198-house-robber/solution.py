43d46a5dca1f141bd6fcf81767becc9cd5d18c06
Kinjal Patel <kpatel.riki@gmail.com>
2025-03-15 06:15:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (82.69%)

--- 0198-house-robber/solution.py
+++ 0198-house-robber/solution.py
@@ -1,5 +1,44 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+
+        return dp[-1]
+        '''
+        Top Down DP
+        n = len(nums)
+        if n == 1:
+                return nums[0]
+
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        dp = [-1] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        def helper(i):
+            if dp[i] != -1:
+                return dp[i]
+            else:
+                dp[i] = max(nums[i]+helper(i-2), helper(i-1))
+                return dp[i]
+
+        return helper(n-1)
+
+        DP Space Optimized
         rob1, rob2 = 0, 0
 
         for n in nums:
@@ -8,3 +47,4 @@ class Solution:
             rob2 = temp
             
         return rob2
+        '''
