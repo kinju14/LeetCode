e4a1b6d8c71d25d2763bd82462b976a38c75f2dd
Kinjal Patel <kpatel.riki@gmail.com>
2025-03-22 18:16:34 +0000

    Sync LeetCode submission Runtime - 50 ms (77.54%), Memory - 32 MB (66.07%)

---
+++ 0053-maximum-subarray/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = float('-inf')
+
+        currSum = 0
+        for n in nums:
+            currSum += n
+            res = max(res, currSum)
+
+            if currSum < 0:
+                currSum = 0
+    
+        return res
+            
+
+
