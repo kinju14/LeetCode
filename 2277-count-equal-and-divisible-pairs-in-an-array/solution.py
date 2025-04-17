f4b5c1fe8f7f41c59a186829f54d7864460e6929
Kinjal Patel <kpatel.riki@gmail.com>
2025-04-17 05:18:29 +0000

    Sync LeetCode submission Runtime - 23 ms (49.67%), Memory - 18 MB (32.33%)

---
+++ 2277-count-equal-and-divisible-pairs-in-an-array/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        res = 0
+
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j] and not (i*j) % k:
+                    res += 1
+
+        return res
+
